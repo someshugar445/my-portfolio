df0188a2e4904ba2cc8cef8557aee63690c10c20
someshugar445 <Kamala@096>
2020-10-18 14:56:16 +0530

    added page links in menu bar

--- projects/urls.py
+++ projects/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path("", views.home, name="home"),
+    path("about/", views.home, name="home"),
     path("", views.project_index, name="project_index"),
     path("<int:pk>/", views.project_detail, name="project_detail"),
 ]

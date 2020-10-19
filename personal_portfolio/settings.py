2055b0d8e67d2fd4e70fab9e0b49a3da8f98c41c
someshugar445 <Kamala@096>
2020-10-19 18:15:00 +0530

    added staticfiles storage

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -123,9 +123,8 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 prod_db = dj_database_url.config(conn_max_age=500)

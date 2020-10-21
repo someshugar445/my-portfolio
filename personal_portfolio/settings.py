5e32409aa1420008b812a019304f78302c02b266
someshugar445 <Kamala@096>
2020-10-21 18:01:15 +0530

    added static files

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -119,7 +119,7 @@ STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'projects','static','img'),
 )
 
 #  Add configuration for static files storage using whitenoise

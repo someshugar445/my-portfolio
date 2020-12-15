45091a7100ad53ecd57d4d9ba6024fb3bd4c7643
Somesh Ugar <47119408+someshugar445@users.noreply.github.com>
2020-12-15 17:48:41 +0530

    updated STATIC_ROOT = "/app/static/"

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "/app/static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
